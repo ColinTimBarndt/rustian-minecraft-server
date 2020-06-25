e37989d8196c6f615f908be62fdf305ad2d31d99
ColinTimBarndt <colin.barndt@gmail.com>
2020-06-25 16:41:55 +0200

    Restructured player data

--- src/packet/play/receive/client_settings.rs
+++ src/packet/play/receive/client_settings.rs
@@ -1,43 +1,49 @@
 use crate::packet::{data::read, PacketParsingError, PacketSerialIn};
-use crate::server::universe::{DisplayedPlayerModelParts, PlayerChatMode, PlayerHand};
+use crate::server::universe::{
+    DisplayedPlayerModelParts, PlayerChatMode, PlayerHand, PlayerSettings,
+};
 
 /// # Client Settings
 /// [Documentation](https://wiki.vg/Protocol#Client_Settings)
 ///
 /// Sent when the player connects, or when settings are changed.
 pub struct ClientSettings {
-    pub locale: String,
-    pub view_distance: u8,
-    pub chat_mode: PlayerChatMode,
-    pub chat_colors: bool,
-    pub displayed_model_parts: DisplayedPlayerModelParts,
-    pub main_hand: PlayerHand,
+    pub settings: PlayerSettings,
 }
 
 impl PacketSerialIn for ClientSettings {
     const ID: u32 = 0x05;
     fn consume_read(mut buffer: Vec<u8>) -> Result<Self, Box<dyn std::error::Error>> {
         use num_traits::FromPrimitive;
+        let locale = read::string(&mut buffer)?;
+        let view_distance = read::u8(&mut buffer)? as u16; // Maybe graphics will become better
+        let chat_mode = match FromPrimitive::from_u32(read::var_u32(&mut buffer)?) {
+            Some(x) => x,
+            None => {
+                return Err(Box::new(PacketParsingError::InvalidPacket(String::from(
+                    format!("Invalid Chat Mode in ClientSettings packet"),
+                ))))
+            }
+        };
+        let chat_colors_enabled = read::bool(&mut buffer)?;
+        let displayed_model_parts = DisplayedPlayerModelParts::new(read::u8(&mut buffer)?);
+        let main_hand = match FromPrimitive::from_u32(read::var_u32(&mut buffer)?) {
+            Some(x) => x,
+            None => {
+                return Err(Box::new(PacketParsingError::InvalidPacket(String::from(
+                    format!("Invalid Main Hand in ClientSettings packet"),
+                ))))
+            }
+        };
+
         Ok(Self {
-            locale: read::string(&mut buffer)?,
-            view_distance: read::u8(&mut buffer)?,
-            chat_mode: match FromPrimitive::from_u32(read::var_u32(&mut buffer)?) {
-                Some(x) => x,
-                None => {
-                    return Err(Box::new(PacketParsingError::InvalidPacket(String::from(
-                        format!("Invalid Chat Mode in ClientSettings packet"),
-                    ))))
-                }
-            },
-            chat_colors: read::bool(&mut buffer)?,
-            displayed_model_parts: DisplayedPlayerModelParts::new(read::u8(&mut buffer)?),
-            main_hand: match FromPrimitive::from_u32(read::var_u32(&mut buffer)?) {
-                Some(x) => x,
-                None => {
-                    return Err(Box::new(PacketParsingError::InvalidPacket(String::from(
-                        format!("Invalid Main Hand in ClientSettings packet"),
-                    ))))
-                }
+            settings: PlayerSettings {
+                locale,
+                view_distance,
+                chat_mode,
+                chat_colors_enabled,
+                displayed_model_parts,
+                main_hand,
             },
         })
     }
@@ -45,15 +51,6 @@ impl PacketSerialIn for ClientSettings {
 
 impl std::fmt::Display for ClientSettings {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
-        write!(
-            f,
-            "locale={}, view_distance={}, chat_mode={}, chat_colors={}, displayed_model_parts={}, main_hand={}",
-            self.locale,
-            self.view_distance,
-            self.chat_mode,
-            self.chat_colors,
-            self.displayed_model_parts,
-            self.main_hand
-        )
+        write!(f, "{}", self.settings)
     }
 }
--- src/server/universe/player.rs
+++ src/server/universe/player.rs
@@ -1,45 +1,65 @@
-use crate::packet::PlayerConnection;
-use super::entity::EntityPlayer;
+use crate::packet::PlayerConnectionPacketHandle;
+use std::default::Default;
+use std::sync::Arc;
+use tokio::sync::RwLock;
 use uuid::Uuid;
+#[macro_use]
+use crate::send_packet;
+
+pub type SharedPlayer = Arc<RwLock<Player>>;
 
 #[derive(Debug)]
 pub struct Player {
-    player_connection: PlayerConnection,
+    player_connection: PlayerConnectionPacketHandle,
     name: String,
     uuid: Uuid,
+    entity_id: u32,
+    pub settings: PlayerSettings,
+    pub player_entity: Option<u32>,
+    selected_hotbar_slot: u8,
+}
+
+#[derive(Debug, Clone)]
+pub struct PlayerSettings {
     pub locale: String,
+    pub view_distance: u16,
     pub chat_mode: PlayerChatMode,
     pub chat_colors_enabled: bool,
     pub displayed_model_parts: DisplayedPlayerModelParts,
     pub main_hand: PlayerHand,
-    pub player_entity: Option<u32>
 }
 
-#[derive(Debug, Clone, num_derive::FromPrimitive)]
+#[derive(Debug, Clone, Copy, num_derive::FromPrimitive)]
 pub enum PlayerChatMode {
-    Enabled = 0, CommandsOnly = 1, Disabled = 2
+    Enabled = 0,
+    CommandsOnly = 1,
+    Disabled = 2,
 }
 
-#[derive(Debug, Clone, num_derive::FromPrimitive)]
+#[derive(Debug, Clone, Copy, num_derive::FromPrimitive)]
 pub enum PlayerHand {
-    Left = 0, Right = 1
+    Left = 0,
+    Right = 1,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 pub struct DisplayedPlayerModelParts(u8);
 
 impl Player {
-    pub fn new(player_connection: PlayerConnection, name: String, uuid: Uuid) -> Self {
+    pub fn new(
+        player_connection: PlayerConnectionPacketHandle,
+        name: String,
+        uuid: Uuid,
+        entity_id: u32,
+    ) -> Self {
         Self {
             player_connection,
             name,
             uuid,
-            locale: String::from("en_US"),
-            chat_mode: PlayerChatMode::Enabled,
-            chat_colors_enabled: true,
-            displayed_model_parts: DisplayedPlayerModelParts::DISPLAY_ALL,
-            main_hand: PlayerHand::Right,
-            player_entity: None
+            entity_id: entity_id,
+            settings: Default::default(),
+            player_entity: None,
+            selected_hotbar_slot: 0,
         }
     }
     pub fn get_name(&self) -> &String {
@@ -48,28 +68,103 @@ impl Player {
     pub fn get_uuid(&self) -> &Uuid {
         &self.uuid
     }
+    pub fn get_entity_id(&self) -> u32 {
+        self.entity_id
+    }
+    pub fn get_selected_hotbar_slot(&self) -> u8 {
+        self.selected_hotbar_slot
+    }
+    pub async fn set_selected_hotbar_slot(&mut self, slot: u8) -> Result<(), String> {
+        let packet = crate::packet::play::send::HeldItemChange { hotbar_slot: slot };
+        let con = &mut self.player_connection;
+        send_packet!(packet => con.send_packet)?;
+        self.selected_hotbar_slot = slot;
+        Ok(())
+    }
+    /// Should only be used when sending the packet manually
+    pub fn set_selected_hotbar_slot_server_side(&mut self, slot: u8) {
+        self.selected_hotbar_slot = slot;
+    }
+}
+
+impl std::fmt::Display for Player {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Player \"{}\" ({})", self.name, self.uuid)
+    }
+}
+
+impl Default for PlayerSettings {
+    fn default() -> Self {
+        Self {
+            locale: "en_US".into(),
+            view_distance: 1,
+            chat_mode: PlayerChatMode::Enabled,
+            chat_colors_enabled: true,
+            displayed_model_parts: DisplayedPlayerModelParts::DISPLAY_ALL,
+            main_hand: PlayerHand::Right,
+        }
+    }
+}
+
+impl std::fmt::Display for PlayerSettings {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        write!(
+            f,
+            "locale={}, view_distance={}, chat_mode={}, chat_colors={}, displayed_model_parts={}, main_hand={}",
+            self.locale,
+            self.view_distance,
+            self.chat_mode,
+            self.chat_colors_enabled,
+            self.displayed_model_parts,
+            self.main_hand
+        )
+    }
 }
 
 impl DisplayedPlayerModelParts {
     pub const DISPLAY_ALL: DisplayedPlayerModelParts = DisplayedPlayerModelParts(0b01111111);
-    const CAPE: u8            = 0b00000001;
-    const JACKET: u8          = 0b00000010;
-    const LEFT_SLEEVE: u8     = 0b00000100;
-    const RIGHT_SLEEVE: u8    = 0b00001000;
-    const LEFT_PANTS_LEG: u8  = 0b00010000;
+    const CAPE: u8 = 0b00000001;
+    const JACKET: u8 = 0b00000010;
+    const LEFT_SLEEVE: u8 = 0b00000100;
+    const RIGHT_SLEEVE: u8 = 0b00001000;
+    const LEFT_PANTS_LEG: u8 = 0b00010000;
     const RIGHT_PANTS_LEG: u8 = 0b00100000;
-    const HAT: u8             = 0b01000000;
+    const HAT: u8 = 0b01000000;
 
-    pub fn new(byte: u8) -> Self {Self(byte)}
-    pub fn to_inner(self) -> u8 {self.0}
+    pub fn new(byte: u8) -> Self {
+        Self(byte)
+    }
+    pub fn to_inner(self) -> u8 {
+        self.0
+    }
 
-    pub fn get_cape(&self)            -> bool {(Self::CAPE & self.0)>0}
-    pub fn get_jacket(&self)          -> bool {(Self::JACKET & self.0)>0}
-    pub fn get_left_sleeve(&self)     -> bool {(Self::LEFT_SLEEVE & self.0)>0}
-    pub fn get_right_sleeve(&self)    -> bool {(Self::RIGHT_SLEEVE & self.0)>0}
-    pub fn get_left_pants_leg(&self)  -> bool {(Self::LEFT_PANTS_LEG & self.0)>0}
-    pub fn get_right_pants_leg(&self) -> bool {(Self::RIGHT_PANTS_LEG & self.0)>0}
-    pub fn get_hat(&self)             -> bool {(Self::HAT & self.0)>0}
+    pub fn get_cape(&self) -> bool {
+        (Self::CAPE & self.0) > 0
+    }
+    pub fn get_jacket(&self) -> bool {
+        (Self::JACKET & self.0) > 0
+    }
+    pub fn get_left_sleeve(&self) -> bool {
+        (Self::LEFT_SLEEVE & self.0) > 0
+    }
+    pub fn get_right_sleeve(&self) -> bool {
+        (Self::RIGHT_SLEEVE & self.0) > 0
+    }
+    pub fn get_left_pants_leg(&self) -> bool {
+        (Self::LEFT_PANTS_LEG & self.0) > 0
+    }
+    pub fn get_right_pants_leg(&self) -> bool {
+        (Self::RIGHT_PANTS_LEG & self.0) > 0
+    }
+    pub fn get_hat(&self) -> bool {
+        (Self::HAT & self.0) > 0
+    }
+}
+
+impl Default for DisplayedPlayerModelParts {
+    fn default() -> Self {
+        Self::DISPLAY_ALL
+    }
 }
 
 impl std::ops::Deref for DisplayedPlayerModelParts {
@@ -84,42 +179,76 @@ impl std::fmt::Display for DisplayedPlayerModelParts {
         let mut displayed = Vec::new();
         let mut hidden = Vec::new();
 
-        if self.get_cape() {displayed.push("cape")}
-        else {hidden.push("cape")};
-        if self.get_jacket() {displayed.push("jacket")}
-        else {hidden.push("jacket")};
-        if self.get_left_sleeve() {displayed.push("left_sleeve")}
-        else {hidden.push("left_sleeve")};
-        if self.get_right_sleeve() {displayed.push("right_sleeve")}
-        else {hidden.push("right_sleeve")};
-        if self.get_left_pants_leg() {displayed.push("left_pants_leg")}
-        else {hidden.push("left_pants_leg")};
-        if self.get_right_pants_leg() {displayed.push("right_pants_leg")}
-        else {hidden.push("right_pants_leg")};
-        if self.get_hat() {displayed.push("hat")}
-        else {hidden.push("hat")};
+        if self.get_cape() {
+            displayed.push("cape")
+        } else {
+            hidden.push("cape")
+        };
+        if self.get_jacket() {
+            displayed.push("jacket")
+        } else {
+            hidden.push("jacket")
+        };
+        if self.get_left_sleeve() {
+            displayed.push("left_sleeve")
+        } else {
+            hidden.push("left_sleeve")
+        };
+        if self.get_right_sleeve() {
+            displayed.push("right_sleeve")
+        } else {
+            hidden.push("right_sleeve")
+        };
+        if self.get_left_pants_leg() {
+            displayed.push("left_pants_leg")
+        } else {
+            hidden.push("left_pants_leg")
+        };
+        if self.get_right_pants_leg() {
+            displayed.push("right_pants_leg")
+        } else {
+            hidden.push("right_pants_leg")
+        };
+        if self.get_hat() {
+            displayed.push("hat")
+        } else {
+            hidden.push("hat")
+        };
 
-        write!(f, "Displayed=[{}], Hidden=[{}]", displayed.join(", "), hidden.join(", "))
+        write!(
+            f,
+            "Displayed=[{}], Hidden=[{}]",
+            displayed.join(", "),
+            hidden.join(", ")
+        )
     }
 }
 
 impl std::fmt::Display for PlayerChatMode {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
         use PlayerChatMode::*;
-        write!(f, "{}", match self {
-            Enabled => "Enabled",
-            CommandsOnly => "Commands Only",
-            Disabled => "Disabled"
-        })
+        write!(
+            f,
+            "{}",
+            match self {
+                Enabled => "Enabled",
+                CommandsOnly => "Commands Only",
+                Disabled => "Disabled",
+            }
+        )
     }
 }
 
 impl std::fmt::Display for PlayerHand {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
         use PlayerHand::*;
-        write!(f, "{}", match self {
-            Left => "Left",
-            Right => "Right"
-        })
+        write!(
+            f,
+            "{}",
+            match self {
+                Left => "Left",
+                Right => "Right",
+            }
+        )
     }
 }
